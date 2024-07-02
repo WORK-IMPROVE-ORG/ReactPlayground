19e7bb0d074717cde75e985fc4936694cb82afbe
jet <a631258@gmail.com>
2024-07-02 09:09:23 +0900

    [이제성] 레이아웃 테스트, ENV 분리 구성, 환경 변수 확인

---
+++ docs/README MEMO.md
@@ -0,0 +1,9 @@
+# MEMO
+메모
+
+## 빌드
+### ENV 분리 구성
+npm install env-cmd
+### env 빌드
+npm run dev:local
+pm2 사용 리서치 필요
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,18 @@
+import { loadEnvConfig } from '@next/env'
 // root '/' URL
+
 export default function Page() {
-  return <h1>HELLO BASE PROJECT</h1>
+  console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+  console.log('process.env.APP_NAME', process.env.APP_NAME);
+  console.log('process.env.NEXT_PUBLIC_APP_ENV', process.env.NEXT_PUBLIC_APP_ENV);
+  console.log('process.env.API_KEY', process.env.API_KEY);
+  return (
+    <div>
+      <h1>HELLO BASE PROJECT</h1>
+      <div>NODE_ENV: [{process.env.NODE_ENV}]</div>
+      <div>APP_NAME: [{process.env.APP_NAME}]</div>
+      <div>APP_ENV: [{process.env.NEXT_PUBLIC_APP_ENV}, {process.env.APP_ENV}]</div>
+      <div>API_KEY: [{process.env.API_KEY}]</div>
+    </div>
+  )
 }
